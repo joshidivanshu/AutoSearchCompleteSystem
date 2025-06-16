8fdcfb5c577cd6e9083bc3fadbc96b1ec047ef30
Divanshu Joshi <divanshujoshi@Divanshus-Laptop.local>
2025-06-16 11:28:09 +0530

    reduced the memory consumption by using backtracking: reduced stringbuilder object creation

--- src/main/java/org/example/TrieNodeService.java
+++ src/main/java/org/example/TrieNodeService.java
@@ -49,17 +49,17 @@ public class TrieNodeService {
         return true;
     }
 
-    public List<Element> fetchRelatedKey(String sentence, TrieNode root) {
+    public List<Element> fetchRelatedKey(String prefix, TrieNode root) {
         List<Element> similarWords = new ArrayList<>();
         TrieNode pCrawl = root;
-        int level, length = sentence.length(), index;
+        int level, length = prefix.length(), index;
         StringBuilder value = new StringBuilder();
         for (level = 0; level < length; level++) {
-            index = sentence.charAt(level) - 'a';
+            index = prefix.charAt(level) - 'a';
             if (pCrawl.getChildren()[index] == null) {
                 return null;
             }
-            value.append(sentence.charAt(level));
+            value.append(prefix.charAt(level));
             pCrawl = pCrawl.getChildren()[index];
         }
         getWordsByDfs(pCrawl, new StringBuilder(value), similarWords);
@@ -70,14 +70,17 @@ public class TrieNodeService {
     public void getWordsByDfs(TrieNode pCrawl, StringBuilder word, List<Element> words) {
         if (pCrawl.isEndOfWord()) {
             words.add(new Element(word.toString(), pCrawl.getFrequency()));
-            return;
         }
         TrieNode[] arr = pCrawl.getChildren();
         for (int i = 0; i < 26; i++) {
-            if (arr[i] != null) {
+            TrieNode childNode = arr[i];
+            if (childNode != null) {
                 word.append((char)(i + 'a'));
-                pCrawl = pCrawl.getChildren()[i];
-                getWordsByDfs(pCrawl, new StringBuilder(word), words);
+
+                // recur
+                getWordsByDfs(childNode, word, words);
+
+                word.deleteCharAt(word.length() - 1);
             }
         }
     }
